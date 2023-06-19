cd8cdc42912c1eef3930303416b4b1b0c232daa3
rehnumatahsin <111407752+rehnumatahsin@users.noreply.github.com>
2023-06-20 01:53:17 +0800

    Add files via upload

---
+++ UTMsradG6_sourceCode/Readme.txt
@@ -0,0 +1,14 @@
+Credential for admin panel :
+username: utmstaff
+password: 12345678
+
+Credential for Existing Accepted User
+email: newuser@gmail.com
+password: 12345678
+
+
+
+http://localhost/UTMsradG6/admin/login.php
+
+
+http://localhost/UTMsradG6/user/login.php
---
+++ UTMsradG6_sourceCode/database/student_data.sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 24, 2022 at 08:16 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmapplications`
+--
+
+CREATE TABLE `tbladmapplications` (
+  `ID` int(11) NOT NULL,
+  `UserId` char(10) NOT NULL,
+  `CourseApplied` varchar(120) DEFAULT NULL,
+  `FatherName` varchar(120) DEFAULT NULL,
+  `MotherName` varchar(120) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `Nationality` varchar(120) DEFAULT NULL,
+  `Gender` varchar(200) DEFAULT NULL,
+  `Category` varchar(200) DEFAULT NULL,
+  `CorrespondenceAdd` varchar(350) NOT NULL,
+  `PermanentAdd` varchar(350) NOT NULL,
+  `SecondaryBoard` varchar(120) DEFAULT NULL,
+  `SecondaryBoardyop` varchar(120) DEFAULT NULL,
+  `SecondaryBoardper` varchar(120) DEFAULT NULL,
+  `SSecondaryBoard` varchar(120) DEFAULT NULL,
+  `SSecondaryBoardyop` varchar(120) DEFAULT NULL,
+  `SSecondaryBoardper` varchar(120) DEFAULT NULL,
+  `GraUni` varchar(120) DEFAULT NULL,
+  `GraUniyop` varchar(120) DEFAULT NULL,
+  `GraUnidper` varchar(120) DEFAULT NULL,
+  `PGUni` varchar(120) DEFAULT NULL,
+  `PGUniyop` varchar(120) DEFAULT NULL,
+  `PGUniper` varchar(120) DEFAULT NULL,
+  `Declaration` varchar(120) DEFAULT NULL,
+  `Signature` varchar(120) DEFAULT NULL,
+  `CourseApplieddate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `AdminRemark` varchar(255) DEFAULT NULL,
+  `AdminStatus` varchar(20) DEFAULT NULL,
+  `AdminRemarkDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `UserPic` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(11) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `AdminuserName` varchar(20) NOT NULL,
+  `MobileNumber` int(10) NOT NULL,
+  `Email` varchar(120) NOT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `AdminuserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(2, 'UTM Staff', 'utmstaff', 1234567897, 'admin@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2022-12-18 04:49:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcourse`
+--
+
+CREATE TABLE `tblcourse` (
+  `ID` int(11) NOT NULL,
+  `CourseName` varchar(90) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblcourse`
+--
+
+INSERT INTO `tblcourse` (`ID`, `CourseName`) VALUES
+(1, 'Bachelor of Architecture'),
+(2, 'Bachelor Of Computer Science (Bioinformatics)'),
+(3, 'Bachelor Of Computer Science (Computer Networks & Security)'),
+(4, 'Bachelor Of Computer Science (Graphics And Multimedia Software)'),
+(5, 'Bachelor Of Computer Science (Industrial Computing)'),
+(6, 'Bachelor Of Computer Science (Software Engineering)'),
+(7, 'Bachelor of Engineering (Chemical)'),
+(9, 'Bachelor of Engineering (Chemical-Bioprocess)'),
+(11, 'Bachelor of Engineering (Civil -Environmental)'),
+(12, 'Bachelor of Engineering (Civil)'),
+(13, 'Bachelor of Engineering (Computer)'),
+(14, 'Bachelor of Engineering (Electrical)'),
+(15, 'Bachelor of Engineering (Mechanical)'),
+(16, 'Bachelor of Engineering (Mechanical-Aeronautics)'),
+(17, 'Foundation Programme'),
+(18, 'Diploma'),
+(19, 'Master'),
+(20, 'PhD');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbldocument`
+--
+
+CREATE TABLE `tbldocument` (
+  `ID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `TransferCertificate` varchar(120) DEFAULT NULL,
+  `TenMarksheeet` varchar(120) DEFAULT NULL,
+  `TwelveMarksheet` varchar(120) DEFAULT NULL,
+  `GraduationCertificate` varchar(120) DEFAULT NULL,
+  `PostgraduationCertificate` varchar(120) DEFAULT NULL,
+  `UploadDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `ID` int(11) NOT NULL,
+  `FirstName` varchar(45) DEFAULT NULL,
+  `LastName` varchar(45) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `Password` varchar(60) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+INSERT INTO `tbluser` (`ID`, `FirstName`, `LastName`, `MobileNumber`, `Email`, `Password`, `PostingDate`) VALUES
+(5, 'New', 'Student', 9905860878, 'newstudent@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2022-12-23 17:57:23');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmapplications`
+--
+ALTER TABLE `tbladmapplications`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcourse`
+--
+ALTER TABLE `tblcourse`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbldocument`
+--
+ALTER TABLE `tbldocument`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmapplications`
+--
+ALTER TABLE `tbladmapplications`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblcourse`
+--
+ALTER TABLE `tblcourse`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tbldocument`
+--
+ALTER TABLE `tbldocument`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
